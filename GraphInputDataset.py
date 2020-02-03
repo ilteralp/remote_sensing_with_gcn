302c194d1936280babf3c7452ee4d9ff81db25a8
ilteralp <milteralp@gmail.com>
2020-02-03 18:21:49 +0300

    Fixed passing processed_file_names() method. Should not be passed

--- GraphInputDataset.py
+++ GraphInputDataset.py
@@ -19,16 +19,19 @@ class GraphInputDataset(InMemoryDataset):
         super(GraphInputDataset, self).__init__(root, transform, pre_transform)
         path = self.processed_paths[0] if train else self.processed_paths[1]
         self.data, self.slices = torch.load(path)
-        
+        #self.data, self.slices = torch.load(self.processed_paths[0])
+
+    # The name of the files to find in the self.raw_dir folder in order to skip the download.
     @property
     def raw_file_names(self):
-        return []
+        return ['train.txt', 'test.txt']
     
+    # A list of files in the processed_dir which needs to be found in order to skip the processing.
     @property
     def processed_file_names(self):
+        return ['data.pt']
         #return ['/home/rog/code/geometric/data/fixed_length.dataset']
-        return ['/home/rog/code/geometric/data/training.pt', 
-                '/home/rog/code/geometric/data/test.pt']
+        #return ['/home/rog/code/geometric/data/training.pt', '/home/rog/code/geometric/data/test.pt']
     
     def download(self):
         pass
@@ -38,28 +41,32 @@ class GraphInputDataset(InMemoryDataset):
     compute slices that will be used by the DataLoader object. 
     """
     def process(self):
-        torch.save(self.process) # Collate olmadan nasil olacak ? 
-        """
         # Read data into huge `Data` list.
         data_list = []
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
-        """
         
 """ ======================= Read dataset ======================= """
 # Constants
 test_id = 143   # Variable length input
 print("Test", test_id)
 ROOT_PATH = "/home/rog/rs/gcn/paths19/test_" + str(test_id) + "/"
+"""
 X_train_feat_path = ROOT_PATH + "train.txt"
 X_test_feat_path = ROOT_PATH + "test.txt"
 info_path = ROOT_PATH +"info.txt"
 
 df_tr = pd.read_csv(X_train_feat_path, header=None)
 df_test = pd.read_csv(X_test_feat_path, header=None)
-
-train_dataset = GraphInputDataset(X_train_feat_path(), train=True)
-test_dataset = GraphInputDataset(X_test_feat_path(), train=False)
+"""
+train_dataset = GraphInputDataset("/home/rog/rs/gcn/paths19/test_143/", train=True)
+#print(train_dataset.processed_dir)
+#test_dataset = GraphInputDataset(ROOT_PATH, train=False)
 
 
 
