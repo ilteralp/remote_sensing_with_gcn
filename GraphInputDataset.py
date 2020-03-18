127ec5d093ac62d86711a7fc3fc293bb2052095d
Melike İlteralp <milteralp@gmail.com>
2020-03-18 17:04:56 +0300

    Removed unused network

--- GraphInputDataset.py
+++ GraphInputDataset.py
@@ -173,43 +173,43 @@ class Net(torch.nn.Module):
         x = self.conv2(x, edge_index)
         return F.log_softmax(x, dim=1) # dim=-1 asagidaki halinde, dim=2 old.icin fark etmez. 
 
-""" ======================================== Build a GNN ======================================== """
-# See https://github.com/rusty1s/pytorch_geometric/blob/master/examples/enzymes_topk_pool.py
+# """ ======================================== Build a GNN ======================================== """
+# # See https://github.com/rusty1s/pytorch_geometric/blob/master/examples/enzymes_topk_pool.py
 
-class Net(torch.nn.Module):
-    def __init__(self):
-        print("network num_features:", train_dataset.num_features)
-        print("network num_classes:", train_dataset.num_classes)
-        super(Net, self).__init__()
-        self.conv1 = GraphConv(train_dataset.num_features, 128)
-        self.pool1 = TopKPooling(128, ratio=0.8)
-        self.conv2 = GraphConv(128, 128)
-        self.pool2 = TopKPooling(128, ratio=0.8)
-        self.conv3 = GraphConv(128, 128)
-        self.pool3 = TopKPooling(128, ratio=0.8)
+# class Net(torch.nn.Module):
+#     def __init__(self):
+#         print("network num_features:", train_dataset.num_features)
+#         print("network num_classes:", train_dataset.num_classes)
+#         super(Net, self).__init__()
+#         self.conv1 = GraphConv(train_dataset.num_features, 128)
+#         self.pool1 = TopKPooling(128, ratio=0.8)
+#         self.conv2 = GraphConv(128, 128)
+#         self.pool2 = TopKPooling(128, ratio=0.8)
+#         self.conv3 = GraphConv(128, 128)
+#         self.pool3 = TopKPooling(128, ratio=0.8)
         
-        self.lin1 = torch.nn.Linear(256, 128)
-        self.lin2 = torch.nn.Linear(128, 64)
-        self.lin3 = torch.nn.Linear(64, train_dataset.num_classes)
+#         self.lin1 = torch.nn.Linear(256, 128)
+#         self.lin2 = torch.nn.Linear(128, 64)
+#         self.lin3 = torch.nn.Linear(64, train_dataset.num_classes)
         
-    def forward(self, data):
-        x, edge_index, batch = data.x, data.edge_index, data.batch
-        x = F.relu(self.conv1(x, edge_index))
-        x, edge_index, _, batch, _, _ = self.pool1(x, edge_index, None, batch)
-        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-        x = F.relu(self.conv2(x, edge_index))
-        x, edge_index, _, batch, _, _ = self.pool2(x, edge_index, None, batch)
-        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-        x = F.relu(self.conv3(x, edge_index))
-        x, edge_index, _, batch, _, _ = self.pool3(x, edge_index, None, batch)
-        x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-        x = x1 + x2 + x3
-        x = F.relu(self.lin1(x))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(self.lin2(x))
-        x = F.log_softmax(self.lin3(x), dim=-1)
+#     def forward(self, data):
+#         x, edge_index, batch = data.x, data.edge_index, data.batch
+#         x = F.relu(self.conv1(x, edge_index))
+#         x, edge_index, _, batch, _, _ = self.pool1(x, edge_index, None, batch)
+#         x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+#         x = F.relu(self.conv2(x, edge_index))
+#         x, edge_index, _, batch, _, _ = self.pool2(x, edge_index, None, batch)
+#         x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+#         x = F.relu(self.conv3(x, edge_index))
+#         x, edge_index, _, batch, _, _ = self.pool3(x, edge_index, None, batch)
+#         x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+#         x = x1 + x2 + x3
+#         x = F.relu(self.lin1(x))
+#         x = F.dropout(x, p=0.5, training=self.training)
+#         x = F.relu(self.lin2(x))
+#         x = F.log_softmax(self.lin3(x), dim=-1)
 
-        return x
+#         return x
     
 #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
@@ -251,7 +251,7 @@ test_loader = DataLoader(test_dataset, batch_size=2)
 #             print('\t', key, val)
 #     print("+" * 100)
 
-for epoch in range(1, 201):
+for epoch in range(1, 3):
     loss = train(epoch)
     train_acc = test(train_loader)
     test_acc = test(test_loader)
