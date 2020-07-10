271b7009fb9772ec89036d1eb1b63e68e9c68b68
Melike İlteralp <milteralp@gmail.com>
2020-07-10 14:02:17 +0300

    Added constants

--- Constants.py
+++ Constants.py
@@ -5,16 +5,27 @@ Created on Fri May 22 02:19:00 2020
 @author: melike
 """
 
+import os
+
+
+NODE_FILE_NAME = '68k_pixel_as_node.txt'
+ALPHA_TRAIN_PERCENT = 0.8
+
 """ ================================ Melike ================================ """
 
-ROOT_PATH = "C:\\Users\\melike\\RS\\gcn\\paths19\\test_149"
-NODE_FILE_NAME = '68k_pixel_as_node.txt'
-
-""" ======================================================================== """
+BASE_PATH = "C:\\Users\\melike\\RS"
 
 """ ================================== ROG ================================= """
 
-# ROOT_PATH = "/home/rog/rs/gcn/paths19/test_149"
-# NODE_FILE_NAME = 'pixel_as_node.txt'
+# BASE_PATH = "/home/rog/rs"
 
 """ ======================================================================== """
+
+
+ROOT_PATH = os.path.join(BASE_PATH, 'gcn', 'paths19', 'test_149')
+VAI_PATH = os.path.join(BASE_PATH, 'vaihingen')
+ALPHA_NODE_FEATS_PATH = os.path.join(VAI_PATH, 'alpha_feats_no_adj.txt')
+ALPHA_EDGES_PATH = os.path.join(VAI_PATH, 'alpha_cliques.txt')
+ALPHA_ROOT_PATH = os.path.join(VAI_PATH, '_pyg')
+
+
